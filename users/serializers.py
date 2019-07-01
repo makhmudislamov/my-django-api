b613e085a1ed7d544c27b2eb731b78c70d548573
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-06-30 21:04:55 -0700

    importing save method - user can login now

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
 from rest_auth.registration.serializers import RegisterSerializer
+from allauth.account.adapter import get_adapter
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,5 +17,22 @@ class CustomeRegisterSerializer(RegisterSerializer):
         fields = ('email', 'username', 'password', 'is_student', 'is_teacher')
 
 # overriding save method
+    def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'email': self.validated_data.get('email', ''),
+            'is_student': self.validated_data.get('is_student', ''),
+            'is_teacher': self.validated_data.get('is_teacher', '')
+        }
+
     def save(self, request):
-        print(request)
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        user.is_student = self.cleaned_data.get('is_student')
+        user.is_teacher = self.cleaned_data.get('is_teacher')
+        user.save()
+        adapter.save_user(request, user, self)
+        return user
