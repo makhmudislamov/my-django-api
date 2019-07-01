f0aa068e657961232d6da911ba75ecd4a349295e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-06-30 19:16:04 -0700

    the user auth page is up - needs bugfix

--- users/models.py
+++ users/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# is your user a teacher or a student?
+class User(AbstractUser):
+    is_student = models.BooleanField()
+    is_teacher = models.BooleanField()
+
+    def __str__(self):
+        return self.username
+
+class Student(models.Model):
+    users = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
